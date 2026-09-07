5dbe4eb1f5e68a4dea0b90d4d67a0bbd9f478bf4
kihernandez <khernandezeuceda@byupathway.edu>
2026-09-07 14:02:16 -0500

    Week 1 Assignments

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,19 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // Step 1: Create an array of doubles with the specified length. 
+        // Step 2: Use a for loop to go through each position in the array. 
+        // Step 3: For each position, multiply the starting number by the position + 1.
+        // Step 4: Store the result in the array.
+        // Step 5: Return the completed array after the loop has finished.
+        
+        double[] multiples = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+
+        return multiples; 
     }
 
     /// <summary>
@@ -29,5 +41,21 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Step 1: Find the index where the list should be split. THis is the length of the list minus the amount of items to move.
+        // Step 2: Use the GetRange method to get the items to move from the end of the list. Saving them in a list.
+        // Step 3: Remove the items to move from the end of the list using the RemoveRange method.
+        // Step 4: Insert the items to move at the beginning of the list using the InsertRange method.
+
+
+        int splitIndex = data.Count - amount;
+
+        List<int> itemsToMove = data.GetRange(splitIndex, amount);
+
+        data.RemoveRange(splitIndex, amount);
+
+        data.InsertRange(0, itemsToMove);
+
+
     }
 }
